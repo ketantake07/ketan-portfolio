44bb1d0c8fb94cee0b9877cd07e59b76104a4a03
Ashok Modhave <ashok.modhave143@gmail.com>
2024-01-06 20:30:56 +1300

    Initial updated commit to create portfolio base

---
+++ src/components/about/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About me</h2>
+      <p>
+        Motivated self-employed developer with an MCA and hands-on experience
+        seeking a challenging role to leverage my full-stack skills and drive
+        impact. I recently completed my MCA from MIT ADT University and dove
+        headfirst into self-employment to actively hone my skills and build
+        tangible solutions. This entrepreneurial spirit led me to projects
+        involving: • Building dynamic web applications with ReactJS and NodeJS,
+        backed by robust databases. • Crafting responsive layouts and
+        interactive UIs with HTML5 and CSS. • Applying a strong understanding of
+        programming languages like Java, JavaScript, C++, and Python to solve
+        real-world problems. I'm a quick learner, a critical thinker, and a
+        passionate problem solver. I thrive in collaborative environments and am
+        eager to contribute to a team where I can continuously learn and grow.
+        Technical Skills: • Programming Languages: Java, JavaScript, ReactJS,
+        C++, Python • Web Development: HTML5, CSS, ReactJS, NodeJS • Databases:
+        MySQL,Oracle • AWS Cloud Technology: AWS S3, DynamoDB, EC2 • Data
+        Exchange protocols: JSON • Tools: Git, GitHub, Figma Let's connect! I'm
+        open to opportunities that align with my skills and ambitions, and ready
+        to accept new challenges.
+      </p>
+      <div>
+        <h3>Skills</h3>
+        <ul>
+          <li>ReactJs</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import About from "../about";
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <h1>Ketan Take portfolio</h1>
+      </div>
+      <About />
+    </div>
+  );
+}
+
+export default App;
